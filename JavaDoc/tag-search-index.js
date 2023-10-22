b36ccb80774a0188faa8f0db620d44dcbb17aa61
glada <glada@volny.cz>
2023-10-22 11:46:03 +0200

    01.00.00

---
+++ JavaDoc/tag-search-index.js
@@ -0,0 +1,1 @@
+tagSearchIndex = [{"l":"The helper for loop planning","h":"class cz.masterart.glada.planner.Planner","d":"Section","u":"cz/masterart/glada/planner/Planner.html#the-helper-for-loop-planning-heading"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cz.masterart.glada.planner","l":"ConditionLine"},{"p":"cz.masterart.glada.planner","l":"ConditionLine.LineBuilder"},{"p":"cz.masterart.glada.planner","l":"Planner"}];updateSearchResults();
